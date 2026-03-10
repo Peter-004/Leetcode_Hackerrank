4ef3b4f095aad57c57b27761e47eb9f9d4081c36
Peter-004 <109190204+Peter-004@users.noreply.github.com>
2026-03-10 14:12:43 -0400

    Update letterCombinations.py
    Improved time complexity by correcting my recursive case

--- letterCombinations.py
+++ letterCombinations.py
@@ -7,33 +7,31 @@ Return the answer in any order.
 A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 '''
 
-def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+def letterCombinations( digits):
+    """
+    :type digits: str
+    :rtype: List[str]
+    """
+    result = []
+    dict = {
+        '2' : ["a","b","c"],
+        '3' : ["d","e","f"],
+        '4' : ["g","h","i"],
+        '5' : ["j","k","l"],
+        '6' : ["m","n","o"],
+        '7' : ["p","q","r","s"],
+        '8' : ["t","u","v"],
+        '9' : ["w","x","y","z"]
+    }
+
+    #base case
+    if (len(digits) == 1):
+        return dict[digits]
+
+    for c in dict[digits[0]]:
+        subList = letterCombinations(digits[1:])
+        for j in subList:
+            result.append(c + j) #if len(c+j) == len(digits) else None
         
-        result = []
-        dict = {
-            '2' : ["a","b","c"],
-            '3' : ["d","e","f"],
-            '4' : ["g","h","i"],
-            '5' : ["j","k","l"],
-            '6' : ["m","n","o"],
-            '7' : ["p","q","r","s"],
-            '8' : ["t","u","v"],
-            '9' : ["w","x","y","z"]
-        }
 
-        #base case
-        if (len(digits) == 1):
-            return dict[digits]
-
-        for i, c in enumerate(digits):
-            for letter in dict[c]:
-                subList = self.letterCombinations(digits[(i+1):])
-                for j in subList:
-                    result.append(letter + j) if len(letter+j) == len(digits) else None
-            
-
-        return result
+    return result
